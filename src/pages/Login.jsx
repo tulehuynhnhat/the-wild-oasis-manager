bea154f39398505929ccdc1dbe0420b08a431341
tulehuynhnhat <tulehuynhnhat05@gmail.com>
2026-02-08 17:02:22 +0700

    Update login ui

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,19 +6,29 @@ import Heading from '../ui/Heading';
 const LoginLayout = styled.main`
   min-height: 100vh;
   display: grid;
-  grid-template-columns: 90%;
+  grid-template-columns: 100%;
   align-content: center;
-  justify-content: center;
-  gap: 3.2rem;
+  justify-items: center;
+  padding: 4rem;
   background-color: var(--color-grey-50);
+`;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 60rem;
+  width: 100%;
+  gap: 3.2rem;
 `;
 
 function Login() {
   return (
     <LoginLayout>
-      <Logo />
-      <Heading as="h4">Log in to your account</Heading>
-      <LoginForm />
+      <Div>
+        <Logo />
+        <Heading as="h4">Log in to your account</Heading>
+        <LoginForm />
+      </Div>
     </LoginLayout>
   );
 }
